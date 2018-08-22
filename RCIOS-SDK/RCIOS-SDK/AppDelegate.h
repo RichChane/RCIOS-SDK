bd3fbf3d3fefd65465fd8e906fbd34396bb1c773
richchane <314112759@qq.com>
2018-08-22 11:50:08 +0800

    ios-sdk

---
+++ RCIOS-SDK/RCIOS-SDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RCIOS-SDK
+//
+//  Created by gzkp on 2018/8/22.
+//  Copyright © 2018年 RC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RCIOS-SDK/RCIOS-SDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RCIOS-SDK
+//
+//  Created by gzkp on 2018/8/22.
+//  Copyright © 2018年 RC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
